1828aa067559c21819ee7a5c977a4496582ebfa8
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-06-04 14:29:16 -0500

    Shawn's school interface four solution with SchoolInterface reformatted

--- classes/interface.py
+++ classes/interface.py
@@ -12,23 +12,32 @@ class SchoolInterface:
             if mode == '1':
                 self.school.list_students()
             elif mode == '2':
-                student_id = input('Enter student id:')
-                student_string = str(self.school.find_student_by_id(student_id))
-                print(student_string)
+                self.view_student()
             elif mode == '3':
-                student_data = {'role':'student'}
-                student_data['name']      = input('Enter student name:\n')
-                student_data['age']       = input('Enter student age: \n')
-                student_data['school_id'] = input('Enter student school id: \n')
-                student_data['password']  = input('Enter student password: \n')
-                
-                self.school.add_student(student_data)
+                self.add_student()
             elif mode == '4':
-                student_id = input("Please enter the student's id:\n")
-                self.school.delete_student(student_id)
+                self.delete_student()
             elif mode == '5':
                 break  
 
 
     def menu(self):
         return "\nWhat would you like to do?\nOptions:\n1. List All Students\n2. View Individual Student <student_id>\n3. Add a Student\n4. Remove a Student <student_id>\n5. Quit\n"
+
+    def view_student(self):
+        student_id = input('Enter student id:')
+        student_string = str(self.school.find_student_by_id(student_id))
+        print(student_string)
+
+    def add_student(self):
+        student_data = {'role':'student'}
+        student_data['name']      = input('Enter student name:\n')
+        student_data['age']       = input('Enter student age: \n')
+        student_data['school_id'] = input('Enter student school id: \n')
+        student_data['password']  = input('Enter student password: \n')
+        
+        self.school.add_student(student_data)
+
+    def delete_student(self):
+        student_id = input("Please enter the student's id:\n")
+        self.school.delete_student(student_id)
